6e3b2cf3971029a039a67f3e52cc68b635a27e4f
MarkHungBuddha <1909kram@gmail.com>
2024-12-05 00:19:28 +0800

    Init commit
    - Personal Blog Basic Frame
    - DB PASSWORD WAIT FOR encryption

---
+++ src/main/java/org/redbuddha/demo/PersonalBlogApplication.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 - redBuddhaHung.
+  */
+package org.redbuddha.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBlogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PersonalBlogApplication.class, args);
+  }
+}
---
+++ src/test/java/org/redbuddha/demo/PersonalBlogApplicationTests.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024 - redBuddhaHung.
+  */
+package org.redbuddha.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
