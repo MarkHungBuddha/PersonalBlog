9ba7bf568b9217933e27409d77ea998e185f4a0c
MarkHungBuddha <1909kram@gmail.com>
2024-12-25 15:16:56 +0800

    feat: WebSecurityConfig create

---
+++ src/main/java/org/redbuddha/personalblog/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2024 - redBuddhaHung.
+  */
+package org.redbuddha.personalblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig {
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    manager.createUser(
+        User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build());
+    return manager;
+  }
+}
